2c17f529096f30a078c476032ccf2020ca70d04f
Audra Tallman <audi.tallman@gmail.com>
2019-03-24 21:51:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
 
+#doesnt forget about the previous total
+# apply_discount method
+  #if inizialized w/ employee discount, then applies the discount to total price, returns success message with updated total and reduces the total
+  #if not using employee discound, then returns a string error message that there is no discount to apply
+# items method returns an array containing all items that have been added
+#void last transaction method that subtracts the last transaction from the total
+
+
+class CashRegister
+  attr_accessor :total, :discount, :item, :last_transaction
+  
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+    @item = []
+    @last_transaction = 0
+  end
+  
+  def add_item(item, price, quantity = 1)
+    self.total = price * quantity
+    quantity.times do
+       @item << item
+    end
+    self.last_transaction = price * quantity
+    end
+  end                   
+  
+  def apply_discount()
+    if discount !=0
+      self.total = (total *((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else 
+      "There is no discount to apply."  
+    end
+  end
+  
+  def item
+    @item
+  end
+
+  def void_last_transaction
+    self.total -= @last_transaction  
+  end
